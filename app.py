adc7663a89c7a37eeeb7328b51167d06e0872f2e
Jennifer Zhang <jennifer.newyork@aol.com>
2018-04-11 12:58:12 -0400

    added base files

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, flash, redirect, url_for, request
+import os
+import json
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()

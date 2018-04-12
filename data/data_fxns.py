1e51e13b91df8be4a2be394223e846769e3267b8
Jennifer Zhang <jennifer.newyork@aol.com>
2018-04-11 21:43:58 -0400

    data functions

---
+++ data/data_fxns.py
@@ -0,0 +1,17 @@
+
+def parse_csv():
+    f = open("USvideos.csv", "r")
+    contents = f.read().split("\n")
+
+    info = []
+    for i in range(0, len(contents)):
+        info.append(contents[i].split(","))
+        if info[i] != '':
+            info[i][4] = info[i][4].split("|")
+    
+        
+    return info
+
+d = parse_csv()
+print d[0]
+print d[1]

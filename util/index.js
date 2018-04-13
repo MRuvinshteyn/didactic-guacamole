e12d1d3616eef3476df2a6a64131c6a42d35b46b
Michael Ruvinshteyn <mruvinshteyn@stuy.edu>
2018-04-13 00:52:53 -0400

    functionalized

--- util/index.js
+++ util/index.js
@@ -2,38 +2,42 @@ var page = "categories"; //which display is currently being shown (categories, v
 var category = null; //which category is being shown -- set to null unless showing a category
 var video = null; //which video is being shown by id -- set to null unless showing a video
 
-var svg = d3.select("svg"),
-    diameter = +svg.attr("width"),
-    g = svg.append("g").attr("transform", "translate(2,2)"),
-    format = d3.format(",d");
+var svg = d3.select("svg")["_groups"][0][0];
 
-var pack = d3.pack()
-.size([diameter - 4, diameter - 4]);
+var displayMenu = function(){
+    var sv = d3.select("svg"),
+        diameter = +sv.attr("width"),
+        g = sv.append("g").attr("transform", "translate(2,2)"),
+        format = d3.format(",d");
 
-d3.json("data.json", function(error, root) {
-    if (error) throw error;
+    var pack = d3.pack()
+    .size([diameter - 4, diameter - 4]);
 
-    root = d3.hierarchy(root)
-        .sum(function(d) { return d.size; })
-        .sort(function(a, b) { return b.value - a.value; });
+    d3.json("data.json", function(error, root) {
+        if (error) throw error;
 
-    var node = g.selectAll(".node")
-    .data(pack(root).descendants())
-    .enter().append("g")
-    .attr("class", function(d) { return d.children ? "node" : "leaf node"; })
-    .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
-    .on("click",function(e){changeCategory(e['data']['name'])});
+        root = d3.hierarchy(root)
+            .sum(function(d) { return d.size; })
+            .sort(function(a, b) { return b.value - a.value; });
 
-    node.append("title")
-        .text(function(d) { return d.data.name + "\n" + format(d.value); });
+        var node = g.selectAll(".node")
+        .data(pack(root).descendants())
+        .enter().append("g")
+        .attr("class", function(d) { return d.children ? "node" : "leaf node"; })
+        .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+        .on("click",function(e){changeCategory(e['data']['name'])});
 
-    node.append("circle")
-        .attr("r", function(d) { return d.r; });
+        node.append("title")
+            .text(function(d) { return d.data.name + "\n" + format(d.value); });
 
-    node.filter(function(d) { return !d.children; }).append("text")
-        .attr("dy", "0.3em")
-        .text(function(d) { return d.data.name.substring(0, d.r / 3); });
-});
+        node.append("circle")
+            .attr("r", function(d) { return d.r; });
+
+        node.filter(function(d) { return !d.children; }).append("text")
+            .attr("dy", "0.3em")
+            .text(function(d) { return d.data.name.substring(0, d.r / 3); });
+    });
+}
 
 //change the category variable based on the input -- activated by clicking on circles
 var changeCategory = function(e){
@@ -52,3 +56,27 @@ var searchByCategory = function(e){
     });
     return arr;
 }
+
+var displayCategory = function(){
+
+}
+
+var displayVideo = function(){
+
+}
+
+//displays different page based on page variable
+var display = function(){
+    svg.innerHTML = "";
+    if (page == "categories"){
+        displayMenu();
+    }
+    if (page == "videos"){
+        displayCategory();
+    }
+    if (page == "info"){
+        displayVideo();
+    }
+}
+
+display();

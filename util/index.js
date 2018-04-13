166a64a1efb3f0b855b200ccf510032ddc48188a
Michael Ruvinshteyn <mruvinshteyn@stuy.edu>
2018-04-12 21:49:52 -0400

    added onClick for circles

--- util/index.js
+++ util/index.js
@@ -4,28 +4,29 @@ var svg = d3.select("svg"),
     format = d3.format(",d");
 
 var pack = d3.pack()
-    .size([diameter - 4, diameter - 4]);
+.size([diameter - 4, diameter - 4]);
 
 d3.json("data.json", function(error, root) {
-  if (error) throw error;
+    if (error) throw error;
 
-  root = d3.hierarchy(root)
-      .sum(function(d) { return d.size; })
-      .sort(function(a, b) { return b.value - a.value; });
+    root = d3.hierarchy(root)
+        .sum(function(d) { return d.size; })
+        .sort(function(a, b) { return b.value - a.value; });
 
-  var node = g.selectAll(".node")
+    var node = g.selectAll(".node")
     .data(pack(root).descendants())
     .enter().append("g")
-      .attr("class", function(d) { return d.children ? "node" : "leaf node"; })
-      .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+    .attr("class", function(d) { return d.children ? "node" : "leaf node"; })
+    .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; })
+    .on("click",function(e){console.log(e['data']['name'])});
 
-  node.append("title")
-      .text(function(d) { return d.data.name + "\n" + format(d.value); });
+    node.append("title")
+        .text(function(d) { return d.data.name + "\n" + format(d.value); });
 
-  node.append("circle")
-      .attr("r", function(d) { return d.r; });
+    node.append("circle")
+        .attr("r", function(d) { return d.r; });
 
-  node.filter(function(d) { return !d.children; }).append("text")
-      .attr("dy", "0.3em")
-      .text(function(d) { return d.data.name.substring(0, d.r / 3); });
+    node.filter(function(d) { return !d.children; }).append("text")
+        .attr("dy", "0.3em")
+        .text(function(d) { return d.data.name.substring(0, d.r / 3); });
 });
